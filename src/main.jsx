807bc9b037df0136f83193837e562cdf9482fe0a
Pedro <phdffonseca@gmaill.com>
2026-02-25 18:18:08 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,14 @@ import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+const routerBasename =
+  import.meta.env.BASE_URL === "/"
+    ? "/"
+    : import.meta.env.BASE_URL.replace(/\/$/, "");
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <HashRouter basename={routerBasename}>
       <App />
     </HashRouter>
   </StrictMode>,
